b2b947e1f8b4c1a186a73da1198a5364d880c06e
Matthias <matthiasludwig@me.com>
2020-12-13 00:03:20 +0100

    Small changes to prepare for backend change

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 const middleware = morgan(':method :url :status :res[content-length] - :response-time ms :body');
 
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
