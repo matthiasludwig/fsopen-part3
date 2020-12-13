af7ce2146de4cbd4cdec31cb1d5a83c6407c1aed
Matthias <matthiasludwig@me.com>
2020-12-13 19:24:24 +0100

    Added first production build of the front-end

---
+++ build/precache-manifest.d96e1d47a541430790d1a5fb20cb7ef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d16f07afc42c093ce0ab78d7e9bb6a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a6169cf2def67ea62fd",
+    "url": "/static/js/2.b14be583.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b14be583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441f27180b70e539bd90",
+    "url": "/static/js/main.42a3ab76.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -36,6 +36,7 @@ morgan.token('body', function (req, res) {
 });
 const middleware = morgan(':method :url :status :res[content-length] - :response-time ms :body');
 
+app.use(express.static('build'));
 app.use(middleware);
 app.use(express.json());
 app.use(cors());
